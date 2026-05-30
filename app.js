9f9453e46ff5f5d68953e6544d83f2e8f213c3e0
david-vynn <davidalvinmaulana@gmail.com>
2026-05-30 08:04:49 +0700

    feat: add to list with add and delete function

---
+++ app.js
@@ -0,0 +1,47 @@
+// 1. Ambil elemen dari HTML
+const taskInput = document.getElementById('taskInput')
+const addBtn = document.getElementById('addBtn')
+const taskList = document.getElementById('taskList')
+
+// 2. Simpan semua tugas di sini
+let tasks = []
+
+// 3. Jalankan fungsi ini saat tombol di klik
+addBtn.addEventListener('click', function() {
+    const taskText = taskInput.value
+
+    // Cek kalau input kosong
+    if (taskText === '') {
+        alert('Tulis tugasnya dulu!')
+        return
+    }
+
+    // Tambah tugas ke array
+    tasks.push(taskText)
+
+    // Kosongkan Input
+    taskInput.value = ''
+
+    // Tampilkan ulang semua tugas
+    renderTasks()
+})
+
+// 4. Fungsi untuk tampilkan tugas ke layar
+function renderTasks() {
+  taskList.innerHTML = ''
+
+  tasks.forEach(function(task, index) {
+    taskList.innerHTML += `
+      <li>
+        <span>${task}</span>
+        <button onclick="deleteTask(${index})">Hapus</button>
+      </li>
+    `
+  })
+}
+
+// 5. Fungsi hapus tugas
+function deleteTask(index) {
+    tasks.splice(index, 1)
+    renderTasks()
+}
